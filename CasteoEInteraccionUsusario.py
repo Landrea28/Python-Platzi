c4a7edc7d01198da94261fc960acd8b464b5d349
Lina Bello <bellolina011@gmail.com>
2025-08-11 17:15:34 -0500

    Como interactúa el usuario

---
+++ CasteoEInteraccionUsusario.py
@@ -0,0 +1,13 @@
+#Interaccion con el usuario
+nombre = input("Ingrese su nombre")
+print(nombre)
+print(type(nombre))
+edad = input("Ingrese su edad")
+print(edad)
+print(type(edad))
+#Si ingresamos input, siempre sera String
+# Toca hacer cateo/ casting :(
+pets = int (input("Ingrese su numero de mascotas"))
+print(type(pets))
+estatura = float (input("Ingrese su estatura"))
+print(type(estatura))
