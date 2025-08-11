13f0f79692607d82a69599c953051437050c62d6
Lina Bello <bellolina011@gmail.com>
2025-08-11 17:13:06 -0500

    Complementando booleanos

---
+++ FlotantesEnterosBooleanos.py
@@ -0,0 +1,41 @@
+#flotantes, enteros y booleanos
+edad = 25
+print (edad)
+print(type(edad))
+
+x = 10
+y = 5.610
+print(type(y))
+z = 10E6
+a = 1.2e6
+b = 1.2e-6
+print(z)
+print(a)
+print(b)
+print(x +x)
+print(x + y)
+print(y + y)
+
+is_true = True
+is_false = False
+print(is_true)
+print(is_false)
+print(type(is_true))
+#bool
+
+
+#Booleanos
+a = 10
+b = 3
+print(a > b) #imprimira true
+print(a < b) #imprimira false
+print("")
+print(a == b) #imprimira false
+print(a != b) #imprimira true
+print("")
+c = 20
+d = 20.0
+print(c > d) #imprimira false
+print(c < d) #imprimira false
+print(c >= d) #imprimira true
+print(c <= d) #imprimira true
