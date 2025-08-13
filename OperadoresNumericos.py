2399ac52591ef11606c80bb2d99d115bd2a65fc0
Lina Bello <bellolina011@gmail.com>
2025-08-13 15:28:33 -0500

    Modifica Operadores Numéricos

---
+++ OperadoresNumericos.py
@@ -0,0 +1,27 @@
+#Operadores numericos
+a = 10
+b = 3
+print("suma", a + b)
+print("resta", a - b)
+print("multiplicacion", a * b)
+print("potencioacion", a ** b) #por ejemplo 10^3
+print("division", a / b)
+print("parte entera de la division", a // b)
+print("modulo", a % b)
+a += 2
+print(a)
+a -= -2
+print(a)
+#PEMDAS (cuando tenemos ejercicios complejos con varios operadores matematicos usamos la regla pemdas)
+#Potenciacion
+#Exponciacion
+#Multiplicacion
+#Divicion
+#Adicion
+#Sustracion
+operacion_1 = 2 + 3
+operacion_2 = (2 + 3) * 4
+operacion_3 = (2 + 3) * (4**2) / 8 + 1
+print(operacion_1)
+print(operacion_2)
+print(operacion_3)
